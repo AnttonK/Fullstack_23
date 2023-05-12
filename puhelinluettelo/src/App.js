389659acdc3bacfb2ac2a4455757bbc180f7301e
AnttonK <antton.koivisto@gmail.com>
2023-05-12 13:36:10 +0300

    2.6-2.7

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: '' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const checkName = persons.some(persons => persons.name === newName)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    console.log(newName)
+    if (checkName) {
+      window.alert(`${newName} is already added to phonebook`)
+      setNewName('')
+    }
+    else {
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    console.log('button clicked', event.target)
+    }
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+//tästä tuu mittään
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName}
+          onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <p>{person.name}</p>)}
+      </ul>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
