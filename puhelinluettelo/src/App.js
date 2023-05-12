fd1ee864d1d9e4acb9e645e7659428370e1941bc
AnttonK <antton.koivisto@gmail.com>
2023-05-12 15:13:15 +0300

    2.12-2.13

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,52 +1,31 @@
-import { useLayoutEffect, useState } from 'react'
-import Person from './components/Person'
-import Filter from './components/Filter'
-import Form from './components/Form'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Person from './components/Person'
+import Form from './components/Form'
+import Filter from './components/Filter'
+import personService from './services/persons'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [ persons, setPersons] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber ] =useState('')
   const [showAll, setShowAll] = useState(false)
-  const [search, setSearch] = useState('')
+  const [search, setSearch] =useState('')
 
-  const checkName = persons.some(persons => persons.name === newName)
-
-  const addPerson = (event) => {
-    event.preventDefault()
-   
-    if (checkName) {
-      window.alert(`${newName} is already added to phonebook`)
-      setNewName('')
-      setNewNumber('')
-    }
-    else {
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length +1
-    }
-    console.log(newName)
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-    console.log('button clicked', event.target)
-    axios
-      .post('http://localhost:3001/persons', personObject)
+  useEffect(() => {
+    personService
+      .getAll()
       .then(response => {
-        console.log(response)
+        console.log('promise fulfilled')
+        setPersons(response.data)
       })
-    }
-  }
-  
+  }, [])
+
+  console.log('render', persons.length, 'persons')
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+
   }
 
   const handleNumberChange = (event) => {
@@ -54,28 +33,63 @@ const App = () => {
   }
 
   const handleSearch = (event) => {
+    console.log(event.target.value)
+    console.log(search)
     setSearch(event.target.value)
   }
- 
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    if (checkName) {
+      window.alert(`${newName} is already added to phonebook`);
+      setNewNumber('')
+      setNewName('')
+    }
+    
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length +1,
+      }
+      
+      personService
+        .create(personObject)
+        .then(response => {
+          console.log(newName)
+          setPersons(persons.concat(personObject))
+          setNewNumber('')
+          setNewName('')
+        })
+    }
+  }
+
+
+  const checkName = persons.some(persons => persons.name.toLowerCase() === newName.toLowerCase())
+
   const personsToShow = showAll
-    ? persons
-    : persons.filter(person => 
-      person.name.toLowerCase().includes(search.toLowerCase()))
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
       <Filter search={search} handleSearch={handleSearch}/>
-      <h2>Add new person</h2>
-      <Form 
-        addPerson ={addPerson} newName={newName}
-        handleNameChange={handleNameChange} newNumber={newNumber}
+      <h2>Add new</h2>
+      <Form
+        addPerson={addPerson} 
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
         handleNumberChange={handleNumberChange}
       />
+        
       <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map((person,i) => <Person key={i} person = {person}/>)}
-      </ul>
+      <div>
+          <ul>{personsToShow.map((person,i) => <Person key={i} person={person} />)}</ul>
+        </div>
     </div>
   )
 
