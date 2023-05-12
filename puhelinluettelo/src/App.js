da8d981d4099308210391677abf6724d71b5b82a
AnttonK <antton.koivisto@gmail.com>
2023-05-12 14:15:49 +0300

    2.8-2.10

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,52 +1,74 @@
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
+import Person from './components/Person'
+import Filter from './components/Filter'
+import Form from './components/Form'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: '' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [search, setSearch] = useState('')
 
   const checkName = persons.some(persons => persons.name === newName)
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName
-    }
-    console.log(newName)
+   
     if (checkName) {
       window.alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
     }
     else {
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length +1
+    }
+    console.log(newName)
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
     console.log('button clicked', event.target)
     }
   }
-
+  
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-//tästä tuu mittään
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+ 
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => 
+      person.name.toLowerCase().includes(search.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName}
-          onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter search={search} handleSearch={handleSearch}/>
+      <h2>Add new person</h2>
+      <Form addPerson ={addPerson} newName={newName}
+        handleNameChange={handleNameChange} newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+        />
+      
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <p>{person.name}</p>)}
+        {personsToShow.map((person,i) => <Person key={i} person = {person}/>)}
       </ul>
     </div>
   )
